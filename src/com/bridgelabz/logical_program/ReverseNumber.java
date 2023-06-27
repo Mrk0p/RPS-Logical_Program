d54d96b4bb9ea3f255e55c6f7b57ac4673b75d23
Mrk0p <sachinwele23@gamil.com>
2023-06-27 08:57:19 +0530

    Added:program for reverse number

---
+++ src/com/bridgelabz/logical_program/ReverseNumber.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.logical_program;
+
+import java.util.Scanner;
+
+public class ReverseNumber {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number :");
+        int num = scanner.nextInt();
+        int reverse = 0;
+
+        while (num != 0) {
+            int digit = num % 10;
+            reverse = reverse * 10 + digit;
+            num = num / 10;
+        }
+        System.out.println("Reverse of a number :"+ reverse);
+    }
+}
